0cb1477886a68ef7755ffd11c4c5d192f3166fab
Nasir Khan Saikat <nasir8891@gmail.com>
2026-09-06 14:19:34 +0600

    feat(flash): add Flash notification class and helper function

---
+++ src/helpers.php
@@ -0,0 +1,10 @@
+<?php
+
+use Nasirkhan\LaravelCube\Support\Flash;
+
+if (! function_exists('flash')) {
+    function flash(string $message = ''): Flash
+    {
+        return new Flash($message);
+    }
+}
