41360c711ddbea0c35a199fc7d016bdd5343098e
Nasir Khan Saikat <nasir8891@gmail.com>
2026-09-19 00:21:28 +0600

    fix(button-link): update baseClasses to use 'leading-none' for consistent text alignment

--- resources/views/components/ui/button-link/tailwind.blade.php
+++ resources/views/components/ui/button-link/tailwind.blade.php
@@ -12,7 +12,7 @@ $variantClasses = match($variant) {
     default => 'bg-gray-800 hover:bg-gray-700 active:bg-gray-900 text-white',
 };
 
-$baseClasses = 'inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs uppercase tracking-widest focus:outline-hidden focus:ring-3 ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 cursor-pointer';
+$baseClasses = 'inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs leading-none uppercase tracking-widest focus:outline-hidden focus:ring-3 ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 cursor-pointer';
 @endphp
 
 <a
